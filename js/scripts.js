55de20aab4817b5309e30724d30732ed4321d108
Nico-Diablo <99472735+Nico-Diablo@users.noreply.github.com>
2022-03-07 12:54:34 +0100

    pokemon object array added

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,25 @@
-alert("Hello world");
+let pokemonList = [
+  {
+    name: "bulbasaur",
+    height: 0.7,
+    types: ["grass", "poison"],
+  },
 
-let favFood = "Pizza";
-document.write(favFood);
+  {
+    name: "jigglypuff",
+    height: 0.5,
+    types: ["normal", "fairy"],
+  },
+
+  {
+    name: "charizard",
+    height: 1.7,
+    types: ["fire", "flying"],
+  },
+
+  {
+    name: "oddish",
+    height: 0.5,
+    types: ["grass", "poison"],
+  },
+];
